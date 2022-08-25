f987833ad031c792432122cac671455bff9fe9b6
Stephen Fabian <fabianstephen@gmail.com>
2022-08-25 13:23:00 -0600

    adds corrected tests for Parent Index sorted by Most Recently Created

--- spec/features/shops/index_spec.rb
+++ spec/features/shops/index_spec.rb
@@ -4,31 +4,40 @@ require 'rails_helper'
 # When I visit the parent index,
 # I see that records are ordered by most recently created first
 # And next to each of the records I see when it was created
+
+
 RSpec.describe 'the Shop Index sorted by Most Recently Created' do #USER STORY 6
+
   describe 'As a visitor' do
+    before :each do
+       @shop = Shop.create!(name: "Denver Bike Shop", rentals: TRUE, rank: 8)
+        @bike = Bike.create!(name: "Bronson", full_suspension: TRUE, travel_length: 150, shop_id: @shop.id)
+    end
+    
     describe 'When I visit the parent index' do
+
+        let(:this) { "Denver Bike Shop" }
+        let(:that) { "OldShop" }
+
       it 'I see that records are ordered by most recently created first' do
-
-        shop = Shop.create!(name: "Denver Bike Shop", rentals: TRUE, rank: 8, created_at: 2022-8-23)
-        shop2 = Shop.create!(name: "Evo", rentals: FALSE, rank: 7, created_at: 2022-8-21)
-        # require 'pry'; binding.pry
-        bike = Bike.create!(name: "Bronson", full_suspension: TRUE, travel_length: 150, shop_id: shop.id)
-        bike2 = Bike.create!(name: "FuelEx", full_suspension: TRUE, travel_length: 130, shop_id: shop2.id)
-
-
-        # let(:this) { "<li>Andrea</li>" }
-        # let(:that) { "<li>Luis</li>" }
+        #OldShop and NewerShop are existing records in shops test db
 
           visit '/shops'
           save_and_open_page
 
-        # expect(this).to appear_before(that)
+          expect(this).to appear_before(that)
+        end
 
-      end
+        it 'And next to each of the records I see when it was created' do 
+        #does this need to be more specific? Test that created_at is NEXT to shop.name
+
+          visit '/shops'
+          save_and_open_page
+     
+          expect(page).to have_content(@shop.name)
+          expect(page).to have_content(@shop.created_at)
+           
+        end
     end
   end
 end
-
-        describe 'And next to each of the records I see when it was created' 
-
-  
