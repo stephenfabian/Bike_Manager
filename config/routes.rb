fee156d244c748cfd3a158b166a60c545baabd2f
Stephen Fabian <fabianstephen@gmail.com>
2022-08-25 17:23:44 -0600

    adds feat Parent Creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get '/shops/new', to: 'shops#new'
     get '/shops', to: 'shops#index'
     get '/shops/:id', to: 'shops#show'
     get '/bikes', to: 'bikes#index'
     get '/bikes/:id', to: 'bikes#show'
     get '/shops/:id/bikes', to: 'shops#children_index'
+
+    post '/shops', to: 'shops#create'
 end
